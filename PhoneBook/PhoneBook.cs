2640b19110138df12fb52231a9eb470db01f7d0d
Gevorg Zhamharyan <gevorgzhamharyan@outlook.com>
2022-08-11 18:11:48 +0400

    Hello

---
+++ PhoneBook/PhoneBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook
+{
+    public class PhoneBook
+    {
+        public string name;
+        public string surname;
+        public char separator;
+        public int phoneNumber; 
+
+        public PhoneBook()
+        {
+
+        }
+
+        public PhoneBook(string name, string surname, char separator, int phoneNumber)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.separator = separator;
+            this.phoneNumber = phoneNumber; 
+        }
+        
+    }
+}
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO; 
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PhoneBook[] pb = new PhoneBook[4];
+
+            pb[0] = new PhoneBook("Edgar", "Danielyan", '/', 0214511111); 
+            pb[1] = new PhoneBook("Hovhannes" , "Shitikyan" , ':' , 01254);
+            pb[2] = new PhoneBook("Artak" , "Hovhannisyan" , ',' , 0988888888);
+            pb[3] = new PhoneBook("Nara" , "Hovhannisyan" , '@' , 012546); 
+
+
+            foreach (var item in pb)
+            {
+                File.WriteAllText("C:\\Results\\PhoneBook.txt",
+                item.name + " " + item.surname + " " + item.separator + " " + item.phoneNumber);
+                string read = File.ReadAllText("C:\\Results\\PhoneBook.txt");
+                Console.WriteLine(read); 
+            }
+
+            Console.WriteLine("\n");
+
+            int count = 0;
+
+            Console.WriteLine("Validations:\n");  
+            for (int i = 0; i < pb.Length; i++) 
+            {
+                Console.Write("line " + i + ":"); 
+                for (int j = pb[i].phoneNumber; j != 0; j /= 10) 
+                {
+                    count++; 
+                }
+                Console.ForegroundColor = ConsoleColor.Red; 
+                Console.WriteLine("Quantity: " + count); 
+                Console.ForegroundColor = ConsoleColor.White; 
+
+                if((pb[i].separator != ':' || pb[i].separator != '-') && count != 9) 
+                {
+                    Console.WriteLine("Phone number should be with 9 digits. Separator should be `:` or `-`. ");
+                } 
+                else if(count != 9)  
+                {
+                    Console.WriteLine("Phone number should be with 9 digits."); 
+                }
+                else if (pb[i].separator != ':' || pb[i].separator != '-')
+                {
+                    Console.WriteLine("Separator should be `:` or `-`. ");
+                }
+                else if((pb[i].separator == ':' || pb[i].separator == '-') && count == 9)
+                {
+                    Console.WriteLine("There is no problem!"); 
+                }
+                Console.WriteLine(); 
+
+                count = 0; 
+            } 
+        }
+    }
+}
