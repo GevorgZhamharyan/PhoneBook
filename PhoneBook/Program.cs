e3144aac7587cd9e662e47da14b92a55645615f0
Gevorg Zhamharyan <gevorgzhamharyan@outlook.com>
2022-08-11 22:47:28 +0400

    phonebook

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -11,7 +11,7 @@ namespace PhoneBook
             PhoneBook[] pb = new PhoneBook[4];
 
             pb[0] = new PhoneBook("Edgar", "Danielyan", '/', 0214511111); 
-            pb[1] = new PhoneBook("Hovhannes" , "Shitikyan" , ':' , 01254);
+            pb[1] = new PhoneBook("Hovhannes" , "Shitikyan" , '*' , 01254); 
             pb[2] = new PhoneBook("Artak" , "Hovhannisyan" , ',' , 0988888888);
             pb[3] = new PhoneBook("Nara" , "Hovhannisyan" , '@' , 012546); 
 
@@ -19,10 +19,12 @@ namespace PhoneBook
             foreach (var item in pb)
             {
                 File.WriteAllText("C:\\Results\\PhoneBook.txt",
-                item.name + " " + item.surname + " " + item.separator + " " + item.phoneNumber);
+                item.name + " " + item.surname + " " + item.separator + " " + item.phoneNumber); 
                 string read = File.ReadAllText("C:\\Results\\PhoneBook.txt");
                 Console.WriteLine(read); 
             }
+
+            
 
             Console.WriteLine("\n");
 
@@ -61,7 +63,43 @@ namespace PhoneBook
                 count = 0;
 
                 Console.WriteLine(); 
-            } 
+
+
+
+                /* Console.Write("Please choose criteria: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                // case 1: --> name
+                // case 2: --> surname
+                // case 3: --> phoneNumberCode 
+
+
+                foreach (var item in pb)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine(item.name);
+                            break;
+
+                        case 2:
+                            Console.WriteLine(item.surname);
+                            break;
+
+                        case 3:
+                            Console.WriteLine(item.phoneNumber.ToString().Substring(0,3)); 
+                            break;
+
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Wrong Input Value!!!Please try again...");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break; 
+                    }
+                } */  
+
+            }
+
         }
     }
 }
